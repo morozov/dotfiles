2f79a4796d45e5838585e3b4361e4476231008e9
Sergei Morozov <morozov@tut.by>
2019-08-21 10:04:55 -0700

    Redefined ls aliases in fish using functions to preserve coloring

--- .fish.d/10-ls.fish
+++ .fish.d/10-ls.fish
@@ -1,3 +1,10 @@
-alias ls='ls --color=auto --literal --group-directories-first'
-alias ll='ls -l'
-alias lla='ls -lA'
+# Additionally, need to patch /usr/share/fish/functions/ls.fish
+# and add `--literal --group-directories-first` to the default parameters.
+# Otherwise, if the function is just overridden, it won't display colors for some reason
+function ll --description 'List contents of directory using long format'
+    ls -l $argv
+end
+
+function lla --description 'List contents of directory using long format including hidden files'
+    ls -lA $argv
+end
