84e0443a54e9d325d517836ce2fcab53a46ebdf4
Sergei Morozov <morozov@tut.by>
2020-10-28 23:40:41 -0700

    Use GNU coreutils instead of this BSD crap

--- .fish.d/02-path.fish
+++ .fish.d/02-path.fish
@@ -1,1 +1,6 @@
 set PATH $PATH $HOME/.local/bin $HOME/Dropbox/SugarCRM/bin
+
+# brew install coreutils
+if [ -d /usr/local/opt/coreutils/libexec/gnubin ]
+    set PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
+end
