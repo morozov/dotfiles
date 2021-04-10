68ecbb42eab0b3d2960a9b0d05a6ac0e63c32676
Sergei Morozov <morozov@tut.by>
2021-04-10 15:20:29 -0700

    Cleanup

--- .fish.d/10-phpunit.fish
+++ .fish.d/10-phpunit.fish
@@ -1,1 +1,1 @@
-alias phpunit="~/Projects/run-without-xdebug/bin/php-noxdebug (command which phpunit) --color=always --columns max --testdox-text /tmp/phpunit-(date +"%Y%m%d-%H%M%S").log"
+alias phpunit="php-noxdebug (command which phpunit) --color=always --columns max --testdox-text /tmp/phpunit-(date +"%Y%m%d-%H%M%S").log"
--- .fish.d/20-go.fish
+++ .fish.d/20-go.fish
@@ -2,3 +2,7 @@ if [ -d /usr/local/go/bin ]
     set -x PATH $PATH /usr/local/go/bin
     set -x GOROOT /usr/local/go
 end
+
+if [ -d $HOME/go/bin ]
+    set -x PATH $PATH $HOME/go/bin
+end
