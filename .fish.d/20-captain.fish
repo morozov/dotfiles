138a2c768906a2af83ad8553ef1261820afa511c
Sergei Morozov <morozov@tut.by>
2021-12-22 17:56:08 -0800

    Improve captain completion

--- .fish.d/20-captain.fish
+++ .fish.d/20-captain.fish
@@ -2,11 +2,10 @@
 if which captain > /dev/null 2>&1
     set -x CAPTAIN_ROOT $HOME/Dropbox/Docker
 
-    function __fish_captain_projects
-        captain ls
-    end
+    complete -f -c captain -n __fish_use_subcommand -a up -d "Start a docker compose project"
+    complete -f -c captain -n __fish_use_subcommand -a down -d "Stop a docker compose project"
+    complete -f -c captain -n __fish_use_subcommand -a ls -d "List available docker compose projects"
 
-    set -l commands up
-    complete -c captain -f -a $commands
-    complete -c captain -n "__fish_seen_subcommand_from up" -a "(captain ls)"
+    complete -f -c captain -n "__fish_seen_subcommand_from up" -a "(captain ls)"
+    complete -f -c captain -n "__fish_seen_subcommand_from down" -a "(captain ls)"
 end
