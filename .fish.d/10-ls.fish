8118b13d691e286b2e7ceb7a00c80a3aca42e7ec
Sergei Morozov <morozov@tut.by>
2021-12-22 17:55:52 -0800

    Improve ls aliases

--- .fish.d/10-ls.fish
+++ .fish.d/10-ls.fish
@@ -1,10 +1,10 @@
-# Additionally, need to patch /usr/share/fish/functions/ls.fish
+# If we wanted to override ls itself, we'd need to patch /usr/share/fish/functions/ls.fish
 # and add `--literal --group-directories-first` to the default parameters.
 # Otherwise, if the function is just overridden, it won't display colors for some reason
 function ll --description 'List contents of directory using long format'
-    ls -l $argv
+    ls -l --literal --group-directories-first $argv
 end
 
 function lla --description 'List contents of directory using long format including hidden files'
-    ls -lA $argv
+    ls -lA --literal --group-directories-first $argv
 end
