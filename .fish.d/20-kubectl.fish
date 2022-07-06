7904b9a0a98be38139d0598bf9c09a04e5004db5
Sergei Morozov <morozov@tut.by>
2022-07-06 10:41:35 -0700

    Replace kubectl with kubecolor

--- .fish.d/20-kubectl.fish
+++ .fish.d/20-kubectl.fish
@@ -2,3 +2,7 @@ if which kubectl > /dev/null 2>&1
     set -x FISH_KUBECTL_COMPLETION_CRD_CACHE_TTL 86400
     alias k=kubectl
 end
+
+if which kubecolor > /dev/null 2>&1
+    alias kubectl=kubecolor
+end
