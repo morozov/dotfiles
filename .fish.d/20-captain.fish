d716cb41d5b1270c66e9a618a080d316b08e703d
Sergei Morozov <morozov@tut.by>
2022-10-19 14:45:32 -0700

    Reduce Captain search depth

--- .fish.d/20-captain.fish
+++ .fish.d/20-captain.fish
@@ -1,6 +1,7 @@
 # See https://github.com/jenssegers/captain
 if which captain > /dev/null 2>&1
     set -x CAPTAIN_ROOT $HOME/Dropbox/Docker
+    set -x CAPTAIN_DEPTH 2
 
     complete -f -c captain -n __fish_use_subcommand -a up -d "Start a docker compose project"
     complete -f -c captain -n __fish_use_subcommand -a down -d "Stop a docker compose project"
