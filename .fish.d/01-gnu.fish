24be001b4e852cfd7fe662a9ec0e73d8cdbba273
Sergei Morozov <morozov@tut.by>
2023-03-09 13:21:30 -0800

    Register gnubin paths on M1

--- .fish.d/01-gnu.fish
+++ .fish.d/01-gnu.fish
@@ -3,12 +3,24 @@ if [ -d /usr/local/opt/gnu-sed/libexec/gnubin ]
     set PATH /usr/local/opt/gnu-sed/libexec/gnubin $PATH
 end
 
+if [ -d /opt/homebrew/opt/gnu-sed/libexec/gnubin ]
+    set PATH /opt/homebrew/opt/gnu-sed/libexec/gnubin $PATH
+end
+
 # brew install grep
 if [ -d /usr/local/opt/grep/libexec/gnubin ]
     set PATH /usr/local/opt/grep/libexec/gnubin $PATH
 end
 
+if [ -d /opt/homebrew/opt/grep/libexec/gnubin ]
+    set PATH /opt/homebrew/opt/grep/libexec/gnubin $PATH
+end
+
 # brew install coreutils
 if [ -d /usr/local/opt/coreutils/libexec/gnubin ]
     set PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
+end
+
+if [ -d /opt/homebrew/opt/coreutils/libexec/gnubin ]
+    set PATH /opt/homebrew/opt/coreutils/libexec/gnubin $PATH
 end
