7d51de8b6584dbe87a5712babaf39ab6f0912b9c
Sergei Morozov <morozov@tut.by>
2023-08-23 09:26:30 -0700

    Disable Docker CLI hints

--- .fish.d/20-docker.fish
+++ .fish.d/20-docker.fish
@@ -2,3 +2,5 @@
 if [ -f /usr/local/opt/docker/share/fish/vendor_completions.d/docker.fish ]
     source /usr/local/opt/docker/share/fish/vendor_completions.d/docker.fish
 end
+
+set -gx DOCKER_CLI_HINTS false
