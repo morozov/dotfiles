b691ee3b84aa2d9901a51de42668bbcc5120d530
Sergei Morozov <morozov@tut.by>
2024-06-28 12:59:47 -0700

    Make AWS CLI help not clear the screen

--- .config/fish/conf.d/20-bat.fish
+++ .config/fish/conf.d/20-bat.fish
@@ -3,5 +3,6 @@ if which batcat > /dev/null 2>&1
 end
 
 if which bat > /dev/null 2>&1
-    set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
+    # See https://rmoff.net/2024/04/26/how-to-stop-aws-cli-clearing-the-screen/
+    set -gx MANPAGER "sh -c 'col -bx | bat -l man -p --pager=\"less -RX\"'"
 end
