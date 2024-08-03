6e1e011de5c913679d558b32d84f8c31cfb168d1
Sergei Morozov <morozov@tut.by>
2024-08-03 11:45:44 -0700

    Natural sort order for ls

--- .config/fish/conf.d/10-ls.fish
+++ .config/fish/conf.d/10-ls.fish
@@ -2,9 +2,9 @@
 # and add `--literal --group-directories-first` to the default parameters.
 # Otherwise, if the function is just overridden, it won't display colors for some reason
 function ll --description 'List contents of directory using long format'
-    ls -l --literal --group-directories-first $argv
+    ls -lv --literal --group-directories-first $argv
 end
 
 function lla --description 'List contents of directory using long format including hidden files'
-    ls -lA --literal --group-directories-first $argv
+    ls -lvA --literal --group-directories-first $argv
 end
