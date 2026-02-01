f464782a14f764f096468a2e79bbd1e7a4d65867
Sergei Morozov <morozov@tut.by>
2026-02-01 11:36:01 -0800

    Add GoLand to $PATH

--- .config/fish/conf.d/20-idea.fish
+++ .config/fish/conf.d/20-idea.fish
@@ -1,5 +1,6 @@
 for p in "/Applications/IntelliJ IDEA.app/Contents/MacOS" \
          "/Applications/CLion.app/Contents/MacOS" \
+         "/Applications/GoLand.app/Contents/MacOS" \
          "/Applications/PhpStorm.app/Contents/MacOS" \
          "/Applications/PyCharm.app/Contents/MacOS" \
          "/Applications/Rider.app/Contents/MacOS"
